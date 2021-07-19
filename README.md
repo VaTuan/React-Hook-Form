1ada217742de81352c3ac133c2fb9e633c43850c
tuanva <vuanhtuan743@gmail.com>
2021-07-19 15:37:55 +0700

    first commit
    created by tuanva 19/07/2021

---
+++ README.md
@@ -0,0 +1,11 @@
+#setup redux, typescript
+
+> yarn create react-app my-app --template redux-typescript
+
+#add react-router-dom
+
+> yarn add react-router-dom @type/react-router-dom
+
+#setup eslint, prettier
+
+>
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "styles/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-body-bold text-xl text-center text-blue-900">Hello</h1>
+    </div>
+  );
+}
+export default App;
